e03a82f685d7d3f96f835980dce61402e33d8b99
Amer Hamdan <amashamdan@gmail.com>
2016-04-25 21:42:03 +0300

    Feat: Added grid using canvas.
    HTML5 canvas used to draw the game's 3x3 grid.

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	/* canvas related stuff should be in document ready, or place at the end
+	of the body in index.html */
+	var grid = document.querySelector("#grid");
+	var ctx = grid.getContext("2d");
+
+	ctx.beginPath();
+	ctx.moveTo(150, 50);
+	ctx.lineTo(150, 350);
+	ctx.moveTo(250, 50);
+	ctx.lineTo(250, 350);
+	ctx.moveTo(50, 150);
+	ctx.lineTo(350, 150);
+	ctx.moveTo(50, 250);
+	ctx.lineTo(350, 250);
+	ctx.lineWidth = 2;
+	ctx.strokeStyle = '#666666';
+	ctx.stroke();
+})
+
